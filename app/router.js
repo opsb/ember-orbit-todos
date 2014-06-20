10ceca280b56d44ef2cc904b1261014fff756f9d
opsb <oliver@opsb.co.uk>
2014-06-20 11:28:00 -0500

    Initial commit

---
+++ app/router.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: OrbitalTodosENV.locationType
+});
+
+Router.map(function() {
+});
+
+export default Router;
---
+++ tests/helpers/resolver.js
@@ -0,0 +1,9 @@
+import Resolver from 'ember/resolver';
+
+var resolver = Resolver.create();
+
+resolver.namespace = {
+  modulePrefix: 'orbital-todos'
+};
+
+export default resolver;
